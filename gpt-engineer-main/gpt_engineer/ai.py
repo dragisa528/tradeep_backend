4e2006c799fc5520bb0bb538d43f2ca6e53e2ad9
techking5287 <techking5287@gmail.com>
2023-08-02 17:22:54 +0300

    delete some files

---
+++ gpt-engineer-main/gpt_engineer/ai.py
@@ -0,0 +1,49 @@
+import openai
+
+
+class AI:
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+
+        try:
+            openai.Model.retrieve("gpt-4")
+        except openai.error.InvalidRequestError:
+            print(
+                "Model gpt-4 not available for provided api key reverting "
+                "to gpt-3.5.turbo. Sign up for the gpt-4 wait list here: "
+                "https://openai.com/waitlist/gpt-4-api"
+            )
+            self.kwargs["model"] = "gpt-3.5-turbo"
+
+    def start(self, system, user):
+        messages = [
+            {"role": "system", "content": system},
+            {"role": "user", "content": user},
+        ]
+
+        return self.next(messages)
+
+    def fsystem(self, msg):
+        return {"role": "system", "content": msg}
+
+    def fuser(self, msg):
+        return {"role": "user", "content": msg}
+
+    def fassistant(self, msg):
+        return {"role": "assistant", "content": msg}
+
+    def next(self, messages: list[dict[str, str]], prompt=None):
+        if prompt:
+            messages = messages + [{"role": "user", "content": prompt}]
+
+        response = openai.ChatCompletion.create(
+            messages=messages, stream=True, **self.kwargs
+        )
+
+        chat = []
+        for chunk in response:
+            delta = chunk["choices"][0]["delta"]
+            msg = delta.get("content", "")
+            print(msg, end="")
+            chat.append(msg)
+        return messages + [{"role": "assistant", "content": "".join(chat)}]
---
+++ gpt-engineer-main/scripts/rerun_edited_message_logs.py
@@ -0,0 +1,38 @@
+import json
+import pathlib
+
+import typer
+
+from ..ai import AI
+from ..chat_to_files import to_files
+
+app = typer.Typer()
+
+
+@app.command()
+def chat(
+    messages_path: str,
+    out_path: str | None = None,
+    model: str = "gpt-4",
+    temperature: float = 0.1,
+    max_tokens: int = 4096,
+):
+    ai = AI(
+        model=model,
+        temperature=temperature,
+        max_tokens=max_tokens,
+    )
+
+    with open(messages_path) as f:
+        messages = json.load(f)
+
+    messages = ai.next(messages)
+
+    if out_path:
+        to_files(messages[-1]["content"], out_path)
+        with open(pathlib.Path(out_path) / "all_output.txt", "w") as f:
+            json.dump(messages[-1]["content"], f)
+
+
+if __name__ == "__main__":
+    app()
