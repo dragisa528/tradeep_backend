21f1e5d4673c09cef15db97c95622507e5720046
techking5287 <techking5287@gmail.com>
2023-08-03 22:16:44 -0700

    update

--- crudexample/urls.py
+++ crudexample/urls.py
@@ -4,11 +4,12 @@ from users import views
 
 urlpatterns = [  
     path('register', views.register),  
+    path('login', views.login),  
     path('admin/', admin.site.urls),  
     # path('emp', views.emp),  
-    path('show',views.show),  
-    path('edit/<int:id>', views.edit),  
-    path('update/<int:id>', views.update),  
-    path('delete/<int:id>', views.destroy),
+    # path('show',views.show),  
+    # path('edit/<int:id>', views.edit),  
+    # path('update/<int:id>', views.update),  
+    # path('delete/<int:id>', views.destroy),
     # url(r'^api-auth/', include('rest_framework.urls'))
 ]  
--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, redirect  
-from users.forms import UserForm  
-from users.models import UserModel  
+from django.shortcuts import render, redirect
+from users.forms import UserForm
+from users.models import UserModel
 from django.http import HttpResponse
 from rest_framework.response import Response
-from rest_framework import status 
+from rest_framework import status
 import json
 from rest_framework import serializers
 from . import models
@@ -11,82 +11,76 @@ from rest_framework.decorators import api_view
 from django.views.decorators.csrf import ensure_csrf_cookie
 import json
 
+
 class MenuSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.UserModel
-        fields = '__all__'
-
-
+        fields = "__all__"
 
 
 @ensure_csrf_cookie
-@api_view(['POST'])
-def register(request): 
+@api_view(["POST"])
+def register(request):
     userload = []
     if request.method == "POST":
         print(request.body)
         temp = json.loads(request.body)
-        if temp['password1'] == temp['password2']:
+        if temp["password1"] == temp["password2"]:
             data = {
-            "upass": temp['password1'],
-            "username": temp['username'],
-            "uid":"111",
-            "email": temp['email']
+                "upass": temp["password1"],
+                "username": temp["username"],
+                "uid": "111",
+                "email": temp["email"],
             }
             tt = json.dumps(data)
-            form = UserForm(data) #json handling for POST 
-            if form.is_valid():  
-                try:  
-                    form.save()  
-                    return HttpResponse("OK") 
+            form = UserForm(data)  # json handling for POST
+            if form.is_valid():
+                try:
+                    form.save()
+                    return HttpResponse("OK")
                 except Exception as e:
-                    print(e)  
-                    pass  
+                    print(e)
+                    pass
             else:
                 return HttpResponse("Form inValid")
-    else:  
-        form = UserForm()  
-    return form 
+    else:
+        form = UserForm()
+    return form
 
 
-# Create your views here.
-# @ensure_csrf_cookie
-# @api_view(['POST'])
-# def emp(request): 
-#     if request.method == "POST":
-#         form = UserForm(json.loads(request.body)) #json handling for POST 
-#         if form.is_valid():  
-#             try:  
-#                 form.save()  
-#                 return HttpResponse("OK") 
-#             except Exception as e:
-#                 print(e)  
-#                 pass  
-#         else:
-#             return HttpResponse("Form inValid")
-#     else:  
-#         form = UserForm()  
-#     return form 
 @ensure_csrf_cookie
-@api_view(['GET'])
-def show(request):  
+@api_view(["POST"])
+def login(request):
+    if request.method == "POST":
+        print(request.body)
+        return 1
+
+@ensure_csrf_cookie
+@api_view(["GET"])
+def show(request):
     employees = UserModel.objects.all()
     serializer = MenuSerializer(employees, many=True)
     return Response(data=serializer.data, status=status.HTTP_200_OK)
-@api_view(['UPDATE']) 
-def edit(request, id):  
-    employee = UserModel.objects.get(uid=id)  
-    return Response(status=status.HTTP_200_OK) 
-@api_view(['UPDATE']) 
-def update(request, id):  
-    employee = UserModel.objects.get(uid=id)  
-    form = UserForm(request.POST, instance = employee)  
-    if form.is_valid():  
-        form.save()  
-        return redirect("/show")  
-    return Response(status=status.HTTP_200_OK)  
-@api_view(['DELETE'])
-def destroy(request, id):  
-    employee = UserModel.objects.get(uid=id)  
-    employee.delete()  
+
+
+@api_view(["UPDATE"])
+def edit(request, id):
+    employee = UserModel.objects.get(uid=id)
+    return Response(status=status.HTTP_200_OK)
+
+
+@api_view(["UPDATE"])
+def update(request, id):
+    employee = UserModel.objects.get(uid=id)
+    form = UserForm(request.POST, instance=employee)
+    if form.is_valid():
+        form.save()
+        return redirect("/show")
+    return Response(status=status.HTTP_200_OK)
+
+
+@api_view(["DELETE"])
+def destroy(request, id):
+    employee = UserModel.objects.get(uid=id)
+    employee.delete()
     return Response(status=status.HTTP_200_OK)
