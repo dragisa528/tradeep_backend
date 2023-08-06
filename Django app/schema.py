a132e41815d10b78bf21028175dbce5f4a5c7349
techking5287 <techking5287@gmail.com>
2023-08-05 23:45:11 -0700

    update-11

--- Django app/schema.py
+++ Django app/schema.py
@@ -1,15 +1,42 @@
+import json
+from django import forms
+from graphene_django import DjangoObjectType, DjangoListField, DjangoMutation
 import graphene
-from graphene_django import DjangoObjectType
+
 from training_api.models import Task
 
-class YourModelType(DjangoObjectType):
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+class TaskType(DjangoObjectType):
     class Meta:
         model = Task
 
+class CreateTaskMutation(DjangoMutation):
+    class Arguments:
+        json_data = graphene.String(required=True)
+
+    task = graphene.Field(TaskType)
+
+    @classmethod
+    def mutate(cls, root, info, json_data):
+        try:
+            data = json.loads(json_data)
+            form = TaskForm(data)
+            if form.is_valid():
+                task = form.save()
+                return CreateTaskMutation(task=task)
+            else:
+                return CreateTaskMutation(errors=form.errors)
+        except json.JSONDecodeError:
+            return CreateTaskMutation(errors={'json_data': 'Invalid JSON format'})
+
+class Mutation(graphene.ObjectType):
+    create_task = CreateTaskMutation.Field()
+
 class Query(graphene.ObjectType):
-    all_yourmodels = graphene.List(Task)
+    tasks = DjangoListField(TaskType)
 
-    def resolve_all_yourmodels(self, info):
-        return Task.objects.all()
-
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
