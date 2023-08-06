af073a23f9170a38beba6d23a81f67cc9ba51335
techking5287 <techking5287@gmail.com>
2023-08-05 23:47:07 -0700

    update-12

--- Django app/schema.py
+++ Django app/schema.py
@@ -14,29 +14,5 @@ class TaskType(DjangoObjectType):
     class Meta:
         model = Task
 
-class CreateTaskMutation(DjangoMutation):
-    class Arguments:
-        json_data = graphene.String(required=True)
-
-    task = graphene.Field(TaskType)
-
-    @classmethod
-    def mutate(cls, root, info, json_data):
-        try:
-            data = json.loads(json_data)
-            form = TaskForm(data)
-            if form.is_valid():
-                task = form.save()
-                return CreateTaskMutation(task=task)
-            else:
-                return CreateTaskMutation(errors=form.errors)
-        except json.JSONDecodeError:
-            return CreateTaskMutation(errors={'json_data': 'Invalid JSON format'})
-
-class Mutation(graphene.ObjectType):
-    create_task = CreateTaskMutation.Field()
-
-class Query(graphene.ObjectType):
-    tasks = DjangoListField(TaskType)
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
