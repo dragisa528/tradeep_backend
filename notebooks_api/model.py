450facba0a731d8cb0e568224d5c34377bd125ca
techking5287 <techking5287@gmail.com>
2023-08-21 23:53:21 -0700

    update-model.py

--- notebooks_api/model.py
+++ notebooks_api/model.py
@@ -16,3 +16,21 @@ class Notebook(models.Model):
 
     def __str__(self):
         return self.title
+    
+    
+    # Define the Notebook model to represent notebooks created by users.
+class Notebook(models.Model):
+    # The title of the notebook.
+    title = models.CharField(max_length=200)
+    # The content of the notebook, stored as text.
+    content = models.TextField()
+    # The owner of the notebook, represented as a foreign key to the User model.
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    # The price of the notebook, stored as a decimal value.
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    # The tags associated with the notebook, represented as a many-to-many relationship with the Tag model.
+    tags = models.ManyToManyField(Tag)
+
+    # String representation of the Notebook model.
+    def __str__(self):
+        return self.title
